6f4a98aa1268b7833d69c632240df614103c31d9
mARCIN <marcinkozikowski@wp.pl>
2018-05-19 15:38:33 +0200

    git push local to origin

---
+++ Algorytmika/Algorithm.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xaml;
+
+namespace Algorytmika
+{
+    class Algorithm
+    {
+        private int startedCity { get; set; } // miasto z ktorego zaczynami i konczymy
+
+        private int quantity { get; set; } // ilosc tras ile mamy w
+
+        private double Dmax { get; set; } // maksymalna trasa w km
+
+        public List<Node> NodesList { get; set; } // lista wszystkich punktow
+        public List<Tuple<Node, Node>> Connections = new List<Tuple<Node, Node>>();
+
+        public double[,] NodeDistances { get; set; } // tablica dystansow.
+
+        public int[] currentSequence;
+
+        private int numberOfNodes;
+
+        public void LoadData(string path)
+        { 
+            NodesList = new List<Node>();
+            using (StreamReader stream = new StreamReader(path))
+            {
+                numberOfNodes = Convert.ToInt32(stream.ReadLine());
+                string line = null;
+                int inc = 0;
+                do
+                {
+                    line = stream.ReadLine();
+                    if (line != null)
+                    {
+                        var data = line.Split(' ');
+                        NodesList.Add(new Node
+                        {
+                            Position = inc++,
+                         //   X = Convert.ToDouble(data[0].Replace('.',',')),
+                         //   Y = Convert.ToDouble(data[1].Replace('.', ',')),
+                            X = Double.Parse(data[0], System.Globalization.NumberStyles.Float, CultureInfo.InvariantCulture),
+                            Y = Double.Parse(data[1], System.Globalization.NumberStyles.Float, CultureInfo.InvariantCulture),
+                            Profit = Convert.ToDouble(data[2]),
+                            Visited = false
+                        });
+                    }
+                } while (line != null);
+            }
+            calcDistances();
+        }
+
+        private void calcDistances()
+        {
+            NodeDistances = new double[NodesList.Count, NodesList.Count];
+            for (int i = 0; i < NodesList.Count; i++)
+            {
+                for (int j = 0; j < NodesList.Count; j++)
+                {
+                    var x = NodesList[i].X - NodesList[j].X;
+                    var y = NodesList[i].Y - NodesList[j].Y;
+                    NodeDistances[i, j] = Convert.ToDouble(Math.Floor(Math.Sqrt(x * x + y * y)));
+                }
+            }
+        }
+
+        private void GreedyRouteConstruction()
+        {
+            List<Node> route = new List<Node>();    //construct route
+            route.Add(NodesList.ElementAt(0));      //add first point to route
+        }
+
+        public void getRoute(double maxDistance, int startPoint)
+        {
+            var currentNode = NodesList.First(p => p.Position == startPoint - 1);
+            var list = new List<int>();
+            currentNode.Visited = true;
+            double currentProfit = currentNode.Profit;
+            var notVisitedNodes = new List<Node>(NodesList);
+            list.Add(currentNode.Position);
+            notVisitedNodes.Remove(currentNode);
+
+            double distance = 0;
+            while (true)
+            {
+                Node nextNode = null;
+                double bestResult = 0;
+                //wez srednio najlepszy punkt.
+                foreach (var notVisitedNode in NodesList)
+                {
+                    if (notVisitedNode.Visited == false)
+                    {
+                        var result = notVisitedNode.Profit /
+                                     NodeDistances[currentNode.Position, notVisitedNode.Position];
+                        if (result > bestResult &&
+                            distance + NodeDistances[currentNode.Position, notVisitedNode.Position] <= maxDistance)
+                        {
+                            bestResult = result;
+                            nextNode = notVisitedNode;
+                        }
+                    }
+                }
+
+                //isc randomowo po bliskich dosc dobrych miastach, i miec ok. pol odleglosci powrotnej w zapasie jak zblizymy sie do tej wartosci wracamy.
+                if (nextNode == null)
+                {
+                    break;
+                }
+
+                nextNode.Visited = true;
+                currentProfit += nextNode.Profit;
+                distance += NodeDistances[currentNode.Position, nextNode.Position];
+                //var pair = new Tuple<Node,Node>(currentNode,nextNode);
+                //Connections.Add(pair);
+                //list.Add(nextNode.Position);
+                currentNode = nextNode;
+            }
+
+            currentSequence = list.ToArray();
+
+        }
+
+        private void TwoOpt(int size, double currentDistance)
+        {
+            
+            var iteration = 0;
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = i; j < size; j++)
+                {
+                    
+                }
+            }
+            
+        }
+
+        public static int[] TwoOptSwap(int[] sequence, int i, int k)
+        {
+            int[] nextSequence = new int[sequence.Length];
+            Array.Copy(sequence, nextSequence, sequence.Length);
+            Array.Reverse(nextSequence, i, k - i + 1);
+
+            return nextSequence;
+        }
+    }
+
+    class Node
+    {
+        public int Position { get; set; }
+        public double X { get; set; }
+        public double Y { get; set; }
+        public double Profit { get; set; }
+        public bool Visited { get; set; }
+    }
+}
---
+++ Algorytmika/MainWindow.xaml.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Algorytmika
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Algorithm alg;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            alg = new Algorithm();
+            CanvasBorder.BorderThickness = new Thickness(1);
+            //alg.LoadData(@"D:\Studia\Algorytmika\Algorytmika\Algorytmika\test.txt");
+         
+        }
+
+        private void OpenFile(object sender, RoutedEventArgs e)
+        {
+            string filePath = "";
+            Microsoft.Win32.OpenFileDialog openFileDialog1 = new Microsoft.Win32.OpenFileDialog();
+
+            openFileDialog1.InitialDirectory = "c:\\";
+            openFileDialog1.Filter = "txt files (*.txt)|*.txt";
+            openFileDialog1.FilterIndex = 2;
+            openFileDialog1.RestoreDirectory = true;
+
+            try
+            {
+                openFileDialog1.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+                if (openFileDialog1.ShowDialog() == true)
+                {
+                    filePath = openFileDialog1.FileName;
+                    alg.LoadData(filePath);
+                    var maxValueX = alg.NodesList.Max(w => w.X) + 2.5;
+                    var maxValueY = alg.NodesList.Max(w => w.Y) + 2.5;
+                    canvas.Height = maxValueY;
+                    canvas.Width = maxValueX;
+                    CanvasBorder.Width = maxValueX + 1;
+                    CanvasBorder.Height = maxValueY + 1;
+                    foreach (var node in alg.NodesList)
+                    {
+                        var ellipse = new Ellipse() { Width = 2.5, Height = 2.5, Stroke = new SolidColorBrush(Colors.Black) };
+                        Canvas.SetLeft(ellipse, node.X);
+                        Canvas.SetTop(ellipse, node.Y);
+                        canvas.Children.Add(ellipse);
+                    }
+                    alg.getRoute(7600, 1);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: Nie można odczytać pliku z danymi: " + ex.Message);
+            }
+        }
+
+        private void ExitAppClick(object sender, RoutedEventArgs e)
+        {
+            System.Windows.Application.Current.Shutdown();
+        }
+    }
+}
